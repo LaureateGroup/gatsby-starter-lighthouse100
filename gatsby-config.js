c8c434288bf7099b44e8de59281bb96177484454
John Milazzo <github@johnmilazzo.com>
2019-02-01 11:42:48 -0600

    shortened short_name.  turned of legacy support of iOS 11.3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: ['UA-'],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared accross all trackingIds
         gtagConfig: {
           anonymize_ip: true
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
           respectDNT: true
         }
       }
@@ -26,12 +20,13 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-lighthouse100.netlify.com',
-        short_name: 'lighthouse100',
+        short_name: 'lighthouse',
         start_url: `/?utm_source=a2hs`,
         background_color: `#fff`,
         theme_color: `#050`,
         display: `standalone`,
-        icon: `src/images/lg-square.jpg`
+        icon: `src/images/lg-square.jpg`,
+        legacy: false
       }
     },
     'gatsby-plugin-netlify',
